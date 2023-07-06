294ac0a651bab6eb61538b5ad390c32a618c64cf
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-07-06 11:50:39 +0100

    Added Service, Controller and Model for Delivery Employee, view to create delivery employee and added endpoint to deliveryEmployeeController

---
+++ controller/deliveryEmployeeController.ts
@@ -0,0 +1,29 @@
+import { Application } from "express"
+import { DeliveryEmployee } from "../model/DeliveryEmployee"
+import { Request } from "express"
+import { Response } from "express"
+
+const deliveryEmployeeService = require('../service/deliveryEmployeeService')
+
+module.exports = function(app: Application) {
+    app.get('/add-delivery-employee', async(req: Request, res: Response) => {
+        res.render('add-delivery-employee');
+    })
+
+    app.post('/add-delivery-employee', async(req:Request, res:Response) => {
+        let data: DeliveryEmployee = req.body;
+        let id: Number
+
+        try {
+            id = await deliveryEmployeeService.createDeliveryEmployee(data)
+
+            res.redirect('/delivery-employees' + id)
+        } catch(e) {
+            console.error(e)
+
+            res.locals.errormessage = e.message
+
+            res.render('add-delivery-employee', req.body)
+        }
+    })
+}
---
+++ model/DeliveryEmployee.ts
@@ -0,0 +1,7 @@
+export class DeliveryEmployee {
+    deliveryEmployeeID: number
+    name: string
+    salary: number
+    bankAccountNo: string
+    natInsuranceNo: string
+}
