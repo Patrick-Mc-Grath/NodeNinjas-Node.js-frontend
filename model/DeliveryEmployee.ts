11bad76d3a4bb09399124788c3f6f6f76d412bd1
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-06 14:12:13 +0100

    Updated delivery employee class to all null

--- model/DeliveryEmployee.ts
+++ model/DeliveryEmployee.ts
@@ -1,7 +1,7 @@
 export class DeliveryEmployee {
-    deliveryEmployeeID: number
-    name: string
-    salary: number
-    bankAccountNo: string
-    natInsuranceNo: string
+  deliveryEmployeeID?: number;
+  name?: string;
+  salary?: number;
+  bankAccountNo?: string;
+  natInsuranceNo?: string;
 }
