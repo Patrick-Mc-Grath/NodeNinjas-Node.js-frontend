b2c2db376b34432a42eccdc22ff5ec83aa531ce0
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-07-06 11:57:04 +0100

    Added deliveryEmployeeValidator and basic validation criteria

---
+++ validator/deliveryEmployeeValidator.ts
@@ -0,0 +1,16 @@
+import { DeliveryEmployee } from "../model/DeliveryEmployee";
+
+module.exports.validateProduct = function (deliveryEmployee: DeliveryEmployee): string {
+    if(deliveryEmployee.name.length > 50) {
+        return "Name must be under 50 characters";
+    }
+    if(deliveryEmployee.salary < 10000) {
+        return "Salary must be at least £10000";
+    }
+    if(deliveryEmployee.bankAccountNo.length > 20) {
+        return "Bank account number must be under 20 characters";
+    }
+    if(deliveryEmployee.natInsuranceNo.length != 9) {
+        return "National insurance nunber must be 9 characters";
+    }
+}
