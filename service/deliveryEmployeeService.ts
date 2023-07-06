4fe90ec311f0221cd023214520e6526d3807fe68
Ieuan Byers <ieuan.byers@kainos.com>
2023-07-06 14:32:19 +0100

    Added function to delivery employee service

--- service/deliveryEmployeeService.ts
+++ service/deliveryEmployeeService.ts
@@ -27,3 +27,13 @@ module.exports.getDeliveryEmployeeByID = async function (id: number): Promise<De
         throw new Error('Could not get delivery employee')
     }
 }
+
+module.exports.updateDeliveryEmployee = async function (id: number, employee: DeliveryEmployee) {
+  try {
+    const response = await axios.put('http://localhost:8080/api/delivery-employee' + id, employee);
+
+    return response.data;
+  } catch (e) {
+    throw new Error('Could not update delivery employee');
+  }
+}
